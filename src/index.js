42e745fa7f42e0f290ecf70f76d224a242a1fbe2
Ana Assis <anvcarolina@outlook.com>
2022-05-26 20:31:14 -0300

    fixed code bug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import GoodMorning from './components/GoodMorning'
-import Multi, { GoodNight } from './components/multiple'
+import { GoodAfternoon, GoodNight } from './components/multiple'
 
 
 
 ReactDOM.render(
     <div>
         <GoodMorning name="Ana"/>
-        <Multi.GoodAfternoon name="Maria"/>
+        <GoodAfternoon name="Maria"/>
         <GoodNight name="Mia"/>
     </div>, document.getElementById('root'))
