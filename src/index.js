4d9c27d5e31a445f395fc88251d5e462a689caf4
Ana Assis <anvcarolina@outlook.com>
2022-05-26 20:10:07 -0300

    multiple components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
-
 import GoodMorning from './components/GoodMorning'
+import Multi, { GoodNight} from './components/multiple'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<GoodMorning name="Maria"/>)
+
+root.render(
+    <div>
+        <GoodMorning name="Ana"/>
+        <Multi.GoodAfternoon name="Maria"/>
+        <GoodNight name="Mia"/>
+    </div>
+)
